b40ace844408468acc3bff2a6c9fe8774b8fd7e1
PriescaLeylya <priescaleylya@gmail.com>
2022-04-05 07:59:15 +0700

    dbsqlite

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:sqlite_flutter/pages/home.dart';
+import 'pages/home.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+//package letak folder Anda
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tambahkan Item => PRIESCA LEYLYA SYAFITRI | 2031710176',
+      title: 'Tambahkan Item',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.blueGrey,
       ),
       home: Home(),
     );
--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,22 +1,30 @@
 class Item {
-  late int _id;
-  late String name;
-  late int price;
+  int _id;
+  String _name;
+  int _price;
 
-  int get id => this._id;
+  int get id => _id;
+  String get name => this._name;
+  int get price => this._price;
 
-  //Konstruktor Versi 1
-  Item({required this.name, required this.price});
+  set name(String value) => this._name = value;
+  set price(value) => this._price = value;
+ 
 
-  //konstruktor versi 2 : konversi dari Map ke Item
+// konstruktor versi 1
+  Item(this._name, this._price);
+// konstruktor versi 2: konversi dari Map ke Item
   Item.fromMap(Map<String, dynamic> map) {
-    _id = map['id'];
-    name = map['name'];
-    price = map['price'];
+    this._id = map['id'];
+    this._name = map['name'];
+    this._price = map['price'];
   }
-
-  //Konversi dari Item ke Map
+  // konversi dari Item ke Map
   Map<String, dynamic> toMap() {
-    return {'id': _id, 'name': name, 'price': price};
+    Map<String, dynamic> map = Map<String, dynamic>();
+    map['id'] = this._id;
+    map['name'] = name;
+    map['price'] = price;
+    return map;
   }
 }
